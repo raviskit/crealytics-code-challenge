0c02a27eccad451c556514ca5f237ee58fb60f52
oholubyev <oholubyev@heliostech.hk>
2015-07-29 10:21:50 +0300

    Moved public method sort(), indented private methods in Modifier class

--- lib/modifier.rb
+++ lib/modifier.rb
@@ -61,80 +61,6 @@ class Modifier
     end
   end
 
-  private
-
-  def combine(merged)
-    result = []
-    merged.each do |_, hash|
-      result << combine_values(hash)
-    end
-    result
-  end
-
-  def combine_values(hash)
-    LAST_VALUE_WINS.each do |key|
-      hash[key] = hash[key].last
-    end
-    LAST_REAL_VALUE_WINS.each do |key|
-      hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
-    end
-    INT_VALUES.each do |key|
-      hash[key] = hash[key][0].to_s
-    end
-    FLOAT_VALUES.each do |key|
-      hash[key] = hash[key][0].from_german_to_f.to_german_s
-    end
-    ['number of commissions'].each do |key|
-      hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
-    end
-    ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'].each do |key|
-      hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
-    end
-    hash
-  end
-
-  def combine_hashes(list_of_rows)
-    keys = []
-    list_of_rows.each do |row|
-      next if row.nil?
-      row.headers.each do |key|
-        keys << key
-      end
-    end
-    result = {}
-    keys.each do |key|
-      result[key] = []
-      list_of_rows.each do |row|
-        result[key] << (row.nil? ? nil : row[key])
-      end
-    end
-    result
-  end
-
-  DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
-
-  def parse(file)
-    CSV.read(file, DEFAULT_CSV_OPTIONS)
-  end
-
-  def lazy_read(file)
-    Enumerator.new do |yielder|
-      CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
-        yielder.yield(row)
-      end
-    end
-  end
-
-  def write(content, headers, output)
-    CSV.open(output, "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
-      csv << headers
-      content.each do |row|
-        csv << row
-      end
-    end
-  end
-
-  public
   def sort(file)
     output = "#{file}.sorted"
     content_as_table = parse(file)
@@ -144,4 +70,77 @@ class Modifier
     write(content, headers, output)
     return output
   end
+
+  private
+
+    def combine(merged)
+      result = []
+      merged.each do |_, hash|
+        result << combine_values(hash)
+      end
+      result
+    end
+
+    def combine_values(hash)
+      LAST_VALUE_WINS.each do |key|
+        hash[key] = hash[key].last
+      end
+      LAST_REAL_VALUE_WINS.each do |key|
+        hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
+      end
+      INT_VALUES.each do |key|
+        hash[key] = hash[key][0].to_s
+      end
+      FLOAT_VALUES.each do |key|
+        hash[key] = hash[key][0].from_german_to_f.to_german_s
+      end
+      ['number of commissions'].each do |key|
+        hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
+      end
+      ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'].each do |key|
+        hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
+      end
+      hash
+    end
+
+    def combine_hashes(list_of_rows)
+      keys = []
+      list_of_rows.each do |row|
+        next if row.nil?
+        row.headers.each do |key|
+          keys << key
+        end
+      end
+      result = {}
+      keys.each do |key|
+        result[key] = []
+        list_of_rows.each do |row|
+          result[key] << (row.nil? ? nil : row[key])
+        end
+      end
+      result
+    end
+
+    DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
+
+    def parse(file)
+      CSV.read(file, DEFAULT_CSV_OPTIONS)
+    end
+
+    def lazy_read(file)
+      Enumerator.new do |yielder|
+        CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
+          yielder.yield(row)
+        end
+      end
+    end
+
+    def write(content, headers, output)
+      CSV.open(output, "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
+        csv << headers
+        content.each do |row|
+          csv << row
+        end
+      end
+    end
 end
