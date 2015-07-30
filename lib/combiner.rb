2931ba4b11113e1e1893dd534593a36670962865
oholubyev <oholubyev@heliostech.hk>
2015-07-30 13:30:18 +0300

    Cleaned up Combiner

--- lib/combiner.rb
+++ lib/combiner.rb
@@ -13,45 +13,32 @@ class Combiner
 
   def combine *enumerators
     Enumerator.new do |yielder|
-      # an array of last values
-      last_values = Array.new(enumerators.size)
+      next_values = Array.new(enumerators.size)
 
-      # done if all enumerators are nil
       done = enumerators.all?(&:nil?)
 
-      # continue until done
       while !done
-
-        # go through all enumerators w/ index
-        last_values.each_with_index do |value, index|
-
-          # if last value is not set, but there is a non-nil enumerator present
+        next_values.each_with_index do |value, index|
           if value.nil? && !enumerators[index].nil?
             begin
-              # set the value to the next item
-              last_values[index] = enumerators[index].next
+              next_values[index] = enumerators[index].next
             rescue StopIteration
-              # reached the end of this enumerator
               enumerators[index] = nil
             end
           end
         end
 
-        # done if all enumerators are nil and there are no last values
-        done = enumerators.all?(&:nil?) && last_values.compact.empty?
-        unless done
-          min_key = get_min_key(last_values)
+        done = enumerators.all?(&:nil?) && next_values.compact.empty?
 
-          # values is the size of enumerators
-          values = Array.new(last_values.size)
-          last_values.each_with_index do |value, index|
+        unless done
+          min_key = get_min_key(next_values)
+          values = Array.new(enumerators.size)
+          next_values.each_with_index do |value, index|
             if key(value) == min_key
               values[index] = value
-              last_values[index] = nil
+              next_values[index] = nil
             end
           end
-
-          # return the values
           yielder.yield(values)
         end
       end
