cc155d236e04a4aa99d54f919d0b88fa43744533
oholubyev <oholubyev@heliostech.hk>
2015-07-30 14:34:44 +0300

    More readable static arrays in Modifier

--- lib/modifier.rb
+++ lib/modifier.rb
@@ -1,12 +1,60 @@
 class Modifier
 
   KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
-  LAST_VALUE_WINS = ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
-  LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
-  INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
-  FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+  LAST_VALUE_WINS =
+    " Account ID
+      Account Name
+      Campaign
+      Ad Group
+      Keyword
+      Keyword Type
+      Subid
+      Paused
+      Max CPC
+      Keyword Unique ID
+      ACCOUNT
+      CAMPAIGN
+      BRAND
+      BRAND+CATEGORY
+      ADGROUP
+      KEYWORD".
+      split("\n").map(&:strip)
+
+  LAST_REAL_VALUE_WINS =
+    " Last Avg CPC
+      Last Avg Pos".
+      split("\n").map(&:strip)
+
+  INT_VALUES =
+    " Clicks
+      Impressions
+      ACCOUNT - Clicks
+      CAMPAIGN - Clicks
+      BRAND - Clicks
+      BRAND+CATEGORY - Clicks
+      ADGROUP - Clicks
+      KEYWORD - Clicks".
+      split("\n").map(&:strip)
+
+  FLOAT_VALUES =
+    " Avg CPC
+      CTR
+      Est EPC
+      newBid
+      Costs
+      Avg Pos".
+      split("\n").map(&:strip)
+
   NUMBER_OF_COMMISSIONS = ['number of commissions']
-  COMMISSION_VALUES = ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
+  COMMISSION_VALUES =
+    " Commission Value
+      ACCOUNT - Commission Value
+      CAMPAIGN - Commission Value
+      BRAND - Commission Value
+      BRAND+CATEGORY - Commission Value
+      ADGROUP - Commission Value
+      KEYWORD - Commission Value".
+      split("\n").map(&:strip)
 
   LINES_PER_FILE = 120000
 
@@ -37,6 +85,7 @@ class Modifier
       end.combine(input_enumerator)
     end
 
+    # merge data according to business logic
     def get_merger combiner
       Enumerator.new do |yielder|
         combiner.each do |list_of_rows|
